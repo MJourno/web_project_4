21bc04d028850e39b692e04ee0526b63c9299b1a
Maayan Journo <maayanjourno@gmail.com>
2021-10-27 22:04:39 +0300

    js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 const formUser = document.querySelector('.profile__value_type_name');
-const formDescription = document.querySelector('.profile__value_type_Description');
+const formDescription = document.querySelector('.profile__value_type_description');
 const formButtonEdit = document.querySelector('.button_edit');
-
 const form = document.querySelector('.popup');
 const formButtonClose = document.querySelector('.popup__close');
+const saveButton = document.querySelector('.popup__save');
+const inputUser = document.querySelector('.popup__input_type_name');
+const inputDescription = document.querySelector('.popup__input_type_description');
 
-formButtonEdit.addEventListener('click', () => {
-  openForm();
 
-});
+formButtonEdit.addEventListener('click', openForm);
 
 formButtonClose.addEventListener('click', closeForm);
 
 function openForm() {
   form.classList.add('popup_is-open');
+  fillInputs ();
 }
+
+function fillInputs () {
+  inputUser.value = formUser.textContent;
+  inputDescription.value = formDescription.textContent;
+  }
 
 function closeForm() {
   form.classList.remove('popup_is-open');
